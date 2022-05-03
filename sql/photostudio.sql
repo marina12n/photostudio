5dca7676246c0bf681c25547a8d4651b489117c4
marina12n <m.d.fedorchuk@gmail.com>
2022-05-03 22:29:37 +0300

    Add sql

--- sql/photostudio.sql
+++ sql/photostudio.sql
@@ -1,83 +1,86 @@
-ALTER TABLE `photostudio`.`client` MODIFY COLUMN `ts` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
+ALTER TABLE `photostudio`.`client` MODIFY COLUMN `date_of_registration` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
 
-INSERT INTO `photostudio`.`camera`
-(`id`, `name`)
+INSERT INTO `photostudio`.`camera` (`id`, `name`)
 VALUES (1 , "Canon"), (2 , "Sony"), (3 , "Nikon");
 
-INSERT INTO `photostudio`.`photographer`
-(`name`, `camera_id`)
-VALUES
-("Bob", 1), ("Alice", 2), ("John", 3);
+INSERT INTO `photostudio`.`administrator` (`name`)
+VALUES ("Valera"), ("Olena"), ("Nick");
 
-select photographer.id, photographer.name as "Name", camera.name as "Camera"
-from photostudio.photographer
-inner join photostudio.camera
-on photographer.camera_id=camera.id;
+INSERT INTO `photostudio`.`photographer` (`name`, `camera_id`)
+VALUES ("Bob", 1), ("Alice", 2), ("John", 3);
 
-INSERT INTO `photostudio`.`event`
-(`name`)
-VALUES ("Wedding"), ("Family"), ("Portrait");
+INSERT INTO `photostudio`.`location` (`name`)
+VALUES("Studio"), ("Field"), ("City");
 
-INSERT INTO `photostudio`.`client`
-(`name`, `phone_number`)
+INSERT INTO `photostudio`.`event` (`name`, `location_id`)
+VALUES ("Wedding", 1), ("Family", 2), ("Portrait", 3);
+
+INSERT INTO `photostudio`.`client` (`name`, `phone_number`)
 VALUES ("Boris", "+380950000000"), ("Joe", "+380981111111"), ("Ursula", "+380972222222");
 
-INSERT INTO `photostudio`.`billing`
-(`payment_type`, `event_id`, `client_id`)
-VALUES
-("Card", 1, 3), ("Cash", 3, 1), ("Card", 2, 2);
+INSERT INTO `photostudio`.`payment_type`(`name`)
+VALUES ("Cash"), ("Card"), ("Crypto");
 
-INSERT INTO `photostudio`.`price_list`
-(`name`, `price`, `event_id`)
-VALUES
-("Standard", "500$", 1), ("Full", "900$", 1), ("Studio", "100$", 2);
+INSERT INTO `photostudio`.`billing` (`event_id`, `client_id`, `payment_type_id`)
+VALUES (1, 3, 1), (3, 1, 2), (2, 2, 3);
 
-INSERT INTO `photostudio`.`schedule`
-(`name`, `photographer_id`, `client_id`)
-VALUES
-("Standart", 1, 2), ("Individual", 2, 3), ("Specail", 3, 8);
+INSERT INTO `photostudio`.`price_list` (`name`, `price`, `event_id`)
+VALUES ("Standard", "500$", 1), ("Full", "900$", 1), ("Studio", "100$", 2);
 
+INSERT INTO `photostudio`.`schedule` (`name`, `photographer_id`, `client_id`, `administrator_id`)
+VALUES ("Standart", 1, 2, 3), ("Individual", 2, 3, 2), ("Specail", 3, 8, 1);
+
+--update--
 UPDATE `photostudio`.`schedule`
-SET
-`name` = "Standard"
+SET `name` = "Standard"
 WHERE `id` = 1;
 
 UPDATE `photostudio`.`billing`
-SET
-`payment_type` = "Cryptocurrency",
-`event_id` = 2,
-`client_id` = 3
-WHERE `client_id` = 4 AND `id` = 4;
+SET `payment_type_id` = 1, `event_id` = 2, `client_id` = 3
+WHERE `client_id` = 4;
 
-select billing.id as "№", billing.payment_type as "Pay", event.name as "Event", client.name as "Client",
-client.phone_number as "Client Phone"
-from photostudio.billing
-inner join photostudio.event
-	on billing.event_id=event.id
- inner join photostudio.client
-	on billing.client_id=client.id;
+UPDATE `photostudio`.`schedule`
+SET `name` = "Special"
+WHERE `id` = 3;
 
-SELECT * FROM photostudio.client ORDER BY ts DESC;
-SELECT * FROM photostudio.client ORDER BY ts;
+--order by--
+SELECT * FROM photostudio.client ORDER BY date_of_registration DESC;
+SELECT * FROM photostudio.client ORDER BY date_of_registration;
 SELECT * FROM photostudio.client WHERE id=2;
 SELECT * FROM photostudio.client WHERE name="Boris";
 
-SELECT client.id as "ID", client.name as "Name", client.phone_number as "Phone", client.ts as "Registration Date"
+--inner join--
+SELECT client.id AS "ID", client.name AS "Name", client.phone_number AS "Phone", client.date_of_registration AS "Registration Date"
 FROM photostudio.client
 LEFT JOIN photostudio.schedule
-on client.id=schedule.client_id
-WHERE schedule.id is NULL;
+ON client.id=schedule.client_id
+WHERE schedule.id IS NULL;
 
-UPDATE `photostudio`.`schedule`
-SET
-`name` = "Special"
-WHERE `id` = 3;
+SELECT billing.id AS "№", payment_type.name AS "Pay", event.name AS "Event", client.name AS "Client",
+client.phone_number AS "Client Phone"
+FROM photostudio.billing
+INNER JOIN photostudio.event
+ON billing.event_id=event.id
+LEFT JOIN photostudio.payment_type
+ON billing.payment_type_id=payment_type.id
+INNER JOIN photostudio.client
+ON billing.client_id=client.id;
 
+SELECT photographer.id, photographer.name AS "Name", camera.name AS "Camera"
+FROM photostudio.photographer
+INNER JOIN photostudio.camera
+ON photographer.camera_id=camera.id
+GROUP BY camera.name;
+
+SELECT * FROM photostudio.photographer
+WHERE photographer.camera_id BETWEEN 2 AND 3;
+
+--delete--
 DELETE FROM `photostudio`.`photographer`
 WHERE camera_id=2;
 
 DELETE FROM `photostudio`.`event`
 WHERE name="Family";
 
-
-
+DELETE FROM photostudio.administrator
+WHERE administrator.name="Nick";.
